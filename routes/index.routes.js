c969e269fe7e2b942b67df7835c4204519185528
Ana <ana.riera@2becommerce.com>
2021-02-28 18:22:51 -0400

    The code has been commented.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,18 +3,23 @@ const utils = require('../utils/utils');
 
 router.use('/people/:sortBy?', async function(req, res) {
     const params = req.params.sortBy !== undefined ? req.params.sortBy : '';
+    // To get all pages
     let people = await utils.getAllPages('people');
     if (params.length > 1) {
+        // For sorting options
         switch(params) {
             case 'name':
                 people.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
                 break;
             case 'height':
+                // To replace the unknown height with "0" value
                 people = people.map(i => {
                     i.height = i.height.replace(/unknown/,"0");
                     return i;
                 });
+                // To sort ascending the people by height
                 people.sort((a,b) => a.height - b.height);
+                // To return its original value of the height field
                 people.forEach(x => {
                     if (x.height === "0") {
                         x.height = "unknown";
@@ -24,18 +29,24 @@ router.use('/people/:sortBy?', async function(req, res) {
             case 'mass':
                 let highMass = '';
                 people = people.map(i => {
+                    // To save the original value of Jabba's mass
                     if (i.mass.includes(',') == true) {
                         highMass = i.mass;
                     }
+                    // To remove the comma-separator in Jabba's mass
                     i.mass = i.mass.replace(/,/g,'');
+                    // To replace the unknown mass with "0" value
                     i.mass = i.mass.replace(/unknown/,"0");
                     return i;
                 });
+                // To sort ascending the people by mass
                 people.sort((a,b) => a.mass - b.mass);
                 people.forEach(x => {
+                    // To return its original value of the mass field
                     if (x.mass === "0") {
                         x.mass = "unknown";
                     }
+                    // To return the original mass to Jabba's field
                     if (x.name.indexOf('Jabba') !== -1) {
                         x.mass = highMass;
                     }
@@ -49,17 +60,21 @@ router.use('/people/:sortBy?', async function(req, res) {
 });
 
 router.use('/planets', async function(req, res) {
+    // To get all pages
     const all = await utils.getAllPages('planets');
     const people = await utils.getAllPages('people');
+    // To filter and get an object with name and url
     const residents = people.map(x => {
         return { "name": x.name, "url": x.url }
     });
     for (const planet of all) {
         let rsdt = [];
         for (const resident of planet.residents) {
+            // To get the match objects
             const match_objs = residents.find(resi => resident === resi.url);
             rsdt.push(match_objs.name);
         }
+        // To asign the names of the residents field to the current planet
         planet.residents = rsdt;
     }
     res.send(all);
--- utils/utils.js
+++ utils/utils.js
@@ -1,28 +1,32 @@
 const axios = require('axios');
 
 module.exports = {  
+    /**
+    * Function Decription: Get Paginate Data from SWAPI
+    * @param {String} endpoint Endpoint to get the data
+    */
     async getAllPages(endpoint) {
         let full_data = [];
-        // first page
+        // First page
         if (endpoint.length > 0) {
             return await axios(`https://swapi.dev/api/${endpoint}`)
             .then(response => {
-                // collect planets from first page
+                // Collect planets from first page
                 full_data = response.data.results;
                 return response.data.count;
             })
             .then(async count => {
-                // exclude the first request
+                // Exclude the first request
                 const numberOfPagesLeft = Math.ceil((count - 1) / 10);
                 let promises = [];
-                // start at 2 as you already queried the first page
+                // Start at 2 as you already queried the first page
                 for (let i = 2; i <= numberOfPagesLeft; i++) {
                     promises.push(await axios(`https://swapi.dev/api/${endpoint}/?page=${i}`));
                 }
                 return Promise.all(promises);
             })
             .then(response => {
-                //get the rest records - pages 2 through n.
+                // Get the rest records - pages 2 through n.
                 full_data = response.reduce((acc, data) => [...acc, ...data.data.results], full_data);
                 return full_data;
             })
